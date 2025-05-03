bb6d48a3ff94d4d83e9a3b16e78d77d493d686e9
Arunachalam_RM <Arunachalam_RM@outlook.com>
2025-05-03 17:58:11 +0530

    Add project files.

---
+++ ClassLib/Product.cs
@@ -0,0 +1,206 @@
+﻿namespace ClassLib
+{
+    public class Product
+    {
+        public double cost;
+        public double tax;
+        private int _productId; //private field
+        public string name;
+        private readonly string description;
+        public int quantityInStock;
+        private double discount;
+        private string productCode;
+        public static string availability;
+
+        public Product(string desc)
+        {
+            description = desc;
+        }
+
+        //manipulating static fields through static method
+        public static void SetAvailability(string value)
+        {
+            availability = value;
+           
+        }
+
+        //manipulating static fields through static method
+        public static string GetAvailability()
+        {
+            return availability;
+        }
+
+        public void SetProductCode(string productCode)
+        {
+            //this keyword-accessible only in instance methods
+            this.productCode = productCode;//accessing current object field
+            this.DummyInstanceMethod();//calling other instance method
+        }
+
+        public string GetproductCode()
+        {
+            return productCode;
+        }
+
+        public double GetDiscount()
+        {
+            return discount;
+        }
+
+        public string GetDescription()//only get method for readonly field
+        {
+            return description;
+        }
+
+        public void SetProductID(int value)//public set method
+        {
+            if (value >= 1 && value <= 100)
+                _productId = value;
+        }
+
+        public int GetProductId()//public get method
+        {
+            return _productId;
+        }
+
+        public double ApplyDiscount(double discount)
+        {
+            return CalDiscount(discount); //calling private method 
+        }
+
+        //private method
+        private double CalDiscount(double disc)//parameter
+        {
+            double FinalCost;//local variable
+            discount = (cost * disc) / 100;
+            FinalCost = cost - discount;
+            return FinalCost;
+        }
+
+        //instance method
+        public void DummyInstanceMethod()
+        {
+            System.Console.WriteLine("Product code was set");
+            //default online mode 
+            availability = "offline";//accessing static field inside instance method
+        }
+
+        //instance method
+        public void InstanceMethod()
+        {
+            System.Console.WriteLine("called Instance method->"+name);
+        }
+
+        //Passing object ref as arguments
+        public static int GetTotalQuantity(Product prod1, Product prod2)
+        {
+            prod1.name = "Samsung";
+            return prod1.quantityInStock + prod2.quantityInStock;
+        }
+
+
+        public void CalculateTax() //instanceMethod
+        {
+            double t;//local variable
+            if (cost > 50000)
+            {
+                t = (cost * 10) / 100;
+            }
+            else
+            {
+                t = (cost * 5) / 100;
+            }
+            tax = t;
+        }
+
+        public void CalculateTax(double perc=5)//default argument(should be used in first)
+        {
+            double t;//local variable
+            if (cost > 50000)
+            {
+                t = (cost * 10) / 100;
+            }
+            else
+            {
+                t = (cost *perc ) / 100;
+            }
+            tax = t;
+        }
+
+
+        public double CalculateTax(int cost, double taxvalue)
+        {
+            tax= (cost * taxvalue) / 100;
+            return tax;
+        }
+
+        public void DefaultParameterModifier(int val)
+        {
+            val = 6;
+            System.Console.WriteLine("Default parameter executed");
+        }
+
+        public void RefParameterModifier(ref int val)
+        {
+            val = 60;
+            System.Console.WriteLine("Ref  parameter modifier executed");
+        }
+
+        public void OutParameterModifier(out int value1,out double value2)
+        {
+            //returning multiple values from method 
+            value1 = 65;
+            value2 = 89;
+            System.Console.WriteLine("out  parameter modifier executed");
+        }
+
+        public void InParameterModifier(in int val)
+        {
+           // val = 60; //parameter modifier of "in" type can't be modified
+            System.Console.WriteLine("In  parameter modifier executed");
+        }
+
+
+        public static void ParamsParameterModifier(params string[] products)
+        {
+            foreach (string product in products)
+            {
+                System.Console.WriteLine(product+" "+product.Length);
+            }
+        }
+
+      
+        public static void DisplayMarks(double cost1,double cost2)
+        {
+            System.Console.WriteLine("Avg cost of :  "+cost1+" and "+cost2+" are "+GetAvgCost());//local function
+
+            double GetAvgCost()
+            {
+                double total = 0;
+                total = (double)(cost1 + cost2) / 2;//default return value is int, typecating to double to get precised value
+                return total;
+            }
+        }
+
+        public static void DisplayMarks2(double cost1, double cost2)
+        {
+            // static local function
+            System.Console.WriteLine("Avg cost of :  " + cost1 + " and " + cost2 + " are " + GetAvgCost(cost1,cost2));
+
+            static double GetAvgCost(double c1,double c2)
+            {
+                double total = 0;
+                total = (double)(c1 + c2) / 2;
+                return total;
+            }
+        }
+
+        public static double Factorial(int number)
+        {
+            if (number == 0)//base case
+                return 1;
+            else
+                return number * Factorial(number - 1);
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,120 @@
+﻿using ClassLib;
+
+class Sample
+{
+    public static void Main(string[] args)
+    {
+        Product laptop = new Product("A quality laptop");
+        laptop.cost = 80000;
+        laptop.name = "HP";
+        laptop.quantityInStock = 2;
+        laptop.SetProductID( 1000);//out of range prodID
+        Console.WriteLine("Laptop prod id->"+laptop.GetProductId());//0
+        laptop.SetProductID(87);//valid prodID
+        Console.WriteLine("Laptop prod id->" + laptop.GetProductId());//87
+        Console.WriteLine(laptop.GetDescription());//A quality laptop
+
+        laptop.CalculateTax();//calling instance method with current object
+        Console.WriteLine("laptop product tax:" + laptop.tax);//8000
+
+        Console.WriteLine("Finalcost:"+laptop.ApplyDiscount(5));//76000
+        Console.WriteLine("discount amount:" + laptop.GetDiscount());//4000
+
+        laptop.SetProductCode("PD1876");
+        System.Console.WriteLine("laptop product code retrieved:"+laptop.GetproductCode());//PD1876
+       // Product.SetAvailability("Online");
+        System.Console.WriteLine("laptop is available in "+Product.GetAvailability()+" mode");
+        laptop.InstanceMethod();//calling instance method
+
+        System.Console.WriteLine();
+        Product mobile = new("A worthy mobile");
+        mobile.cost = 20000;
+        mobile.name = "Iphone";
+        mobile.quantityInStock = 1;
+        Product.SetAvailability("Offline");
+        System.Console.WriteLine("mobile is available in " + Product.GetAvailability() + " mode");
+        mobile.InstanceMethod();//calling instance method
+
+        System.Console.WriteLine();
+        System.Console.WriteLine("*******default arguments*****:");
+        mobile.CalculateTax();//default argument
+        System.Console.WriteLine("calculated tax for mobile:"+mobile.tax);//1000
+
+        System.Console.WriteLine();
+        System.Console.WriteLine("*******named arguments*****:");
+        mobile.CalculateTax(perc: 1);//improves readbility
+        System.Console.WriteLine("calculated tax for mobile:" + mobile.tax);//200
+
+        System.Console.WriteLine();
+        System.Console.Write("*******Passing object ref as arguments*****:");
+          System.Console.WriteLine("\nbefore modification of mobile name->" + mobile.name);//Iphone
+        int totalQuantityInStock=Product.GetTotalQuantity(mobile,laptop);
+        System.Console.WriteLine(totalQuantityInStock);//3
+        System.Console.Write("after modification of mobile name->" + mobile.name);//Samsung
+
+        System.Console.WriteLine();
+        System.Console.WriteLine("****MethodOverloading***");
+        Product Watch = new Product("Stunning watch");
+        System.Console.WriteLine("calculate tax for watch->" + Watch.CalculateTax(10000, 10));//1000
+        System.Console.WriteLine("Tax for watch:" + Watch.tax);//1000
+        Watch.cost = 1000;
+        Watch.CalculateTax();//50
+        System.Console.WriteLine("Tax for watch:"+Watch.tax);//50
+
+        System.Console.WriteLine();
+        System.Console.WriteLine("***Parameter Modifier-Default***");
+        int a=9;
+        System.Console.WriteLine("Before modified 'A' value will be:" + a);
+        Watch.DefaultParameterModifier(a);
+        System.Console.WriteLine("After modified 'A' value will be:" + a);
+
+        System.Console.WriteLine();
+        System.Console.WriteLine("***Parameter Modifier-Ref***");
+        int b = 9;
+        System.Console.WriteLine("Before modified 'B' value will be:" + b);
+        Watch.RefParameterModifier(ref b);
+        System.Console.WriteLine("After modified 'B' value will be:" + b);
+
+        System.Console.WriteLine();
+        System.Console.WriteLine("***Parameter Modifier-Out***");
+        int c ;
+        System.Console.WriteLine("Before modified 'C' value is not initialized");
+        Watch.OutParameterModifier(out c,out double c2);
+        System.Console.WriteLine("After modified 'C' value will be:" + c);
+        System.Console.WriteLine("'C2' value will be:" + c2);
+
+        System.Console.WriteLine();
+        System.Console.WriteLine("***Parameter Modifier-In***");
+        int d = 9;
+        System.Console.WriteLine("Before modified 'D' value will be:" + d);
+        Watch.InParameterModifier(in d);
+        System.Console.WriteLine("After modified 'D' value will be:" + d);
+
+        System.Console.WriteLine();
+        System.Console.WriteLine("printing products list 1:");
+        string[] productsList1 = { "toy", "sofa", "furniture" };
+        Product.ParamsParameterModifier(productsList1);
+        System.Console.WriteLine();
+        System.Console.WriteLine("printing products list 2:");
+        string[] productsList2 = { "bench", "chair", "tablet","keyboard" };
+        Product.ParamsParameterModifier(productsList2);
+
+        System.Console.WriteLine();
+        System.Console.WriteLine("local function::");
+        Product.DisplayMarks(34, 35);//34.5
+
+        System.Console.WriteLine();
+        System.Console.WriteLine("static local function::");
+        Product.DisplayMarks2(23, 24);//23.5
+
+        System.Console.WriteLine();
+        System.Console.WriteLine("Recursion::");
+        System.Console.WriteLine("Factorial of 5 is "+Product.Factorial(5));//120
+        System.Console.WriteLine("Factorial of 6 is " + Product.Factorial(6));//720
+
+
+
+
+
+    }
+}
